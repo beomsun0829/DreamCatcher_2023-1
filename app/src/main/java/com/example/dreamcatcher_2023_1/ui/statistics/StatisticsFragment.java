94ee9244373858742130342ac159d14cd6f8a18f
Hong Beomsun <ghsqjatns@gmail.com>
2023-05-28 16:16:20 +0900

    modify Statistics layout, fix scroll bug

--- app/src/main/java/com/example/dreamcatcher_2023_1/ui/statistics/StatisticsFragment.java
+++ app/src/main/java/com/example/dreamcatcher_2023_1/ui/statistics/StatisticsFragment.java
@@ -13,14 +13,15 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.dreamcatcher_2023_1.R;
 import com.example.dreamcatcher_2023_1.databinding.FragmentStatisticsBinding;
-import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
 
 import java.util.ArrayList;
 
@@ -38,30 +39,38 @@ public class StatisticsFragment extends Fragment {
 
         // Example data
         TextView sleepTime = root.findViewById(R.id.sleepTime);
-        sleepTime.setText("Sleep Time: 8 hours");
+        sleepTime.setText("8 hours");
 
         TextView sleepRegularity = root.findViewById(R.id.sleepRegularity);
-        sleepRegularity.setText("Sleep Regularity: 90%");
+        sleepRegularity.setText("90%");
 
-        TextView timeToFallAsleep = root.findViewById(R.id.timeToFallAsleep);
-        timeToFallAsleep.setText("Time to Fall Asleep: 15 minutes");
+        TextView timeToFallAsleep = root.findViewById(R.id.timeToSleep);
+        timeToFallAsleep.setText("15 min");
 
         TextView bedTime = root.findViewById(R.id.bedTime);
-        bedTime.setText("Bed Time: 10:00 PM");
+        bedTime.setText("10:00 PM");
 
         TextView wakeUpTime = root.findViewById(R.id.wakeUpTime);
-        wakeUpTime.setText("Wake Up Time: 6:00 AM");
+        wakeUpTime.setText("6:00 AM");
 
         // PieChart
         PieChart pieChart = root.findViewById(R.id.pieChart);
         ArrayList<PieEntry> pieEntries = new ArrayList<>();
-        pieEntries.add(new PieEntry(8f, "Sleep"));
-        pieEntries.add(new PieEntry(16f, "Awake"));
-        PieDataSet pieDataSet = new PieDataSet(pieEntries, "Daily Sleep Time");
+        pieEntries.add(new PieEntry(8f, ""));
+        pieEntries.add(new PieEntry(16f, ""));
+        PieDataSet pieDataSet = new PieDataSet(pieEntries, "");
         pieDataSet.setColors(new int[] { Color.BLUE, Color.GRAY });
         PieData pieData = new PieData(pieDataSet);
         pieChart.setData(pieData);
+
+        // Hide labels
+        pieChart.setDrawEntryLabels(false);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.getLegend().setEnabled(false);
+
         pieChart.invalidate(); // refresh
+
+
 
         // LineChart
         LineChart lineChart = root.findViewById(R.id.lineChart);
@@ -71,11 +80,27 @@ public class StatisticsFragment extends Fragment {
         lineEntries.add(new Entry(2f, 8f));
         lineEntries.add(new Entry(3f, 6f));
         lineEntries.add(new Entry(4f, 7f));
-        LineDataSet lineDataSet = new LineDataSet(lineEntries, "Sleep Conditions");
+        LineDataSet lineDataSet = new LineDataSet(lineEntries, "");
         lineDataSet.setColor(Color.BLUE);
         LineData lineData = new LineData(lineDataSet);
         lineChart.setData(lineData);
+
+    // Hide labels, gridlines, and axis lines
+        lineChart.getXAxis().setDrawGridLines(false);
+        lineChart.getXAxis().setDrawAxisLine(false);
+        lineChart.getXAxis().setDrawLabels(false);
+        lineChart.getAxisLeft().setDrawGridLines(false);
+        lineChart.getAxisLeft().setDrawAxisLine(false);
+        lineChart.getAxisLeft().setDrawLabels(false);
+        lineChart.getAxisRight().setDrawGridLines(false);
+        lineChart.getAxisRight().setDrawAxisLine(false);
+        lineChart.getAxisRight().setDrawLabels(false);
+        lineChart.getDescription().setEnabled(false);
+        lineChart.getLegend().setEnabled(false);
+
         lineChart.invalidate(); // refresh
+
+
 
         return root;
     }
