a53ba046aec07e70537dc9570663f40de83d4722
Hong Beomsun <ghsqjatns@gmail.com>
2023-06-07 19:24:33 +0900

    update statistics ui and function

--- app/src/main/java/com/example/dreamcatcher_2023_1/ui/user/UserFragment.java
+++ app/src/main/java/com/example/dreamcatcher_2023_1/ui/user/UserFragment.java
@@ -15,8 +15,11 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.dreamcatcher_2023_1.databinding.FragmentUserBinding;
 
-public class UserFragment extends Fragment {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class UserFragment extends Fragment {
     private FragmentUserBinding binding;
     private boolean isDarkModeEnabled = false;
 
@@ -27,8 +30,6 @@ public class UserFragment extends Fragment {
 
         binding = FragmentUserBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-
         binding.foregroundSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -42,14 +43,53 @@ public class UserFragment extends Fragment {
             }
         });
 
+        binding.debugButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                debugSleepDataFile();
+            }
+        });
+
 
         return root;
     }
-
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void debugSleepDataFile(){
+        File file = new File(getActivity().getFilesDir(), "sleep_data.txt");
+
+        try {
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write("[\n" +
+                    "  {\n" +
+                    "    \"date\": \"20230605\",\n" +
+                    "    \"startHours\": 23,\n" +
+                    "    \"startMinutes\": 38,\n" +
+                    "    \"endHours\": 8,\n" +
+                    "    \"endMinutes\": 22,\n" +
+                    "    \"memo\": \"great\",\n" +
+                    "    \"rating\": 3\n" +
+                    "  },\n" +
+                    "  {\n" +
+                    "    \"date\": \"20230606\",\n" +
+                    "    \"startHours\": 22,\n" +
+                    "    \"startMinutes\": 59,\n" +
+                    "    \"endHours\": 7,\n" +
+                    "    \"endMinutes\": 21,\n" +
+                    "    \"memo\": \"nice\",\n" +
+                    "    \"rating\": 3\n" +
+                    "  }\n" +
+                    "]\n");
+            fileWriter.close();
+            Toast.makeText(getActivity(), "Sleep data file modified", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(getActivity(), "Failed to modify sleep data file", Toast.LENGTH_SHORT).show();
+        }
     }
 }
